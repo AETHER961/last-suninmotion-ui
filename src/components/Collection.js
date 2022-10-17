741bbcd805f9e69713ebdf5fe304e32dd49d55c0
Claude Massaad <cnm07@mai.aub.edu>
2022-10-17 15:52:56 +0300

    added images so that all resolutions work

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -5,6 +5,7 @@ import Img1 from "../assets/insects/1.png";
 import Img2 from "../assets/insects/2.png";
 import Img3 from "../assets/insects/3.png";
 import Img4 from "../assets/insects/4.png";
+import Img5 from "../assets/insects/5.png";
 
 import useMobileType from "../hooks/useMobileType";
 import CollectionSlider from "./CollectionSlider";
@@ -14,6 +15,8 @@ import Bg2 from "../assets/dynaicc.svg";
 import Bg3 from "../assets/dynaiccc.svg";
 import Bg4 from "../assets/dynaicccc.svg";
 import Bg5 from "../assets/dynaiccccc.svg";
+import Bg6 from "../assets/dynaicccccc.svg";
+
 const Collection = () => {
   const { isMobile } = useMobileType();
 
@@ -63,7 +66,7 @@ const Collection = () => {
     { bg: "bg3", img: Img2, backgrounds: Bg3 },
     { bg: "bg4", img: Img3, backgrounds: Bg4 },
     { bg: "bg5", img: Img4, backgrounds: Bg5 },
-    { bg: "bg3", img: Img2, backgrounds: Bg3 },
+    { bg: "bg6", img: Img5, backgrounds: Bg6 },
   ];
 
   return (
